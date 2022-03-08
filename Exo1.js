bc42ef64a32370de9a9ada713a4f7c876fc68afc
Nicolas Houegbelo <Nikolajuwon@gmail.com>
2022-03-08 21:41:43 +0100

    workshop

---
+++ Exo1.js
@@ -0,0 +1,21 @@
+let age = 29;
+
+let nom = "Nicolas Houegbelo";
+let eleve = false;
+const ecole = "3i School";
+
+console.log(typeof age);
+console.log(typeof nom);
+console.log(typeof eleve);
+console.log(typeof ecole);
+
+console.log(age);
+console.log(nom);
+console.log(eleve);
+console.log(ecole);
+
+age = 30 + 1;
+eleve = true;
+
+console.log(age);
+console.log(eleve);
